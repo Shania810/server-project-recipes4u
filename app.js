e00a8c892f9da317399c4691d64eaf85dd4bc39f
Deny Nakano <deny.nakano@gmail.com>
2023-01-19 23:05:26 -0300

    recipe routes add to app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const connectionDb = require('./db/db.config.js');
+const recipeRoutes = require('./Routes/recipes.routes.js');
 
 connectionDb();
 
-app.get('/',(req,res)=> res.send('oi'))
+app.use('/', recipeRoutes);
 
 module.exports = app
