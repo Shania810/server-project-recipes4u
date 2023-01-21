ac7d0cb75eb29978bed8a71bc68922ff91a658af
Deny Nakano <deny.nakano@gmail.com>
2023-01-21 18:13:16 -0300

    insert route in app.js

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const connectionDb = require('./db/db.config.js');
 const recipeRoutes = require('./Routes/recipes.routes.js');
+const bestRecipesRoutes = require('./Routes/bestRecipes.routes.js');
 
 
 connectionDb();
 
-app.use(express.json())
+app.use(express.json());
 app.use('/', recipeRoutes);
+app.use('/', bestRecipesRoutes);
 
 
 module.exports = app
