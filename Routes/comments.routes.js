00f22151d5d611620369b6f9bb657c80921865a3
Shania <shaniaantezana@gmail.com>
2023-01-22 15:08:00 -0400

    chnaged comments.routes.js

--- Routes/comments.routes.js
+++ Routes/comments.routes.js
@@ -5,18 +5,21 @@ const Comment = require("../Models/Comments.Models.js");
 
 
 router.post("/comment/:idRecipe", async (req, res) => {
-    const{idRecipe} = req.params
+    const { idRecipe } = req.params
     const comment = req.body
     try {
-        const newComment = await Comment.create({...comment,recipe:idRecipe})
-        const commentsRecipe = await Comment.find({recipe: idRecipe})
-        const ratesRecipe = commentsRecipe.map(({rate})=>rate)
-        const averageRatesRecipe = Math.round(ratesRecipe.reduce((acc,cu)=> acc + cu,0) / ratesRecipe.length)
-        await Recipe.findByIdAndUpdate(idRecipe,{rate: averageRatesRecipe},{new: true})
-        res.status(200).json(newComment)
+        const newComment = await Comment.create({ ...comment, recipe: idRecipe })
+
+        const commentsRecipe = await Comment.find({ recipe: idRecipe })
+        const ratesRecipe = commentsRecipe.map(({ rate }) => rate)
+        const averageRatesRecipe = Math.round(ratesRecipe.reduce((acc, cu) => acc + cu, 0) / ratesRecipe.length)
+
+        await Recipe.findByIdAndUpdate(idRecipe, { rate: averageRatesRecipe }, { new: true })
+        const updateRecipe = await Recipe.findByIdAndUpdate(idRecipe, { $push: { comments: newComment } }, { new: true })
+        res.status(200).json(updateRecipe)
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 });
 
-module.exports= router
+module.exports = router
