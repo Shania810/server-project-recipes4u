a7f2ad4e85b2cfd927be19d9c9b1b0032a763e5e
Shania <shaniaantezana@gmail.com>
2023-01-25 19:22:27 -0400

    installed dependencies

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
 const connectionDb = require('./db/db.config.js');
 const recipeRoutes = require('./Routes/recipes.routes.js');
 const bestRecipesRoutes = require('./Routes/bestRecipes.routes.js');
@@ -7,7 +9,8 @@ const commentRoutes = require('./routes/comments.routes.js');
 
 
 connectionDb();
-
+app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
 app.use('/', recipeRoutes);
 app.use('/', bestRecipesRoutes);
